dbc76e06e800a2b84acd0193f80e12e3adc60bbf
zhangbiqiong <zhang.biqiong@qq.com>
2019-12-19 06:43:35 +0800

    Update output.py

--- output.py
+++ output.py
@@ -1,4 +1,3 @@
-
 # import time
 import numpy as np
 # import mxnet as mx
@@ -50,50 +49,70 @@ mydict = {
 }
 
 lines=[]
-row=1800
+row=10000
 for n in range(1,row):#row
     line=[]
     for m in range(1,50):
         line.append(mydict[random.randint(1,24)])
     lines.append(line)
 
-for line in lines[0:int(row/3)]:
+groupsize=int(row/5)
+for line in lines[0:int(groupsize)]:
     n=random.randint(10,40)
     line[n]='X'
-    # line[n+1]='Z'
     line[n+random.randint(1,3)]='Y'
 
-for line in lines[int(row/3):int(row/3)*2]:
+for line in lines[groupsize:groupsize*2]:
     n=random.randint(10,40)
     line[n]='M'
-    # line[n+1]='Z'
     line[n+random.randint(1,3)]='N'
 
-for line in lines[int(row/3)*2:]:
+for line in lines[groupsize*2:groupsize*3]:
     n=random.randint(10,40)
     line[n]='S'
-    # line[n+1]='Z'
     line[n+random.randint(1,3)]='T'
 
+for line in lines[groupsize*3:groupsize*4]:
+    n=random.randint(10,40)
+    line[n]='U'
+    line[n+random.randint(1,3)]='V'
+
+for line in lines[groupsize*4:]:
+    n=random.randint(10,40)
+    line[n]='P'
+    line[n+random.randint(1,3)]='Q'
 
 np.savetxt('a.txt',lines,fmt='%s')
 # print (lines)    
 
-model = Word2Vec(
-    sentences=lines,  # We will supply the pre-processed list of moive lists to this parameter
-    iter=900,  # epoch
-    min_count=10,  # a movie has to appear more than 10 times to be keeped
-    size=1000,  # size of the hidden layer
-    workers=10,  # specify the number of threads to be used for training
-    sg=1,  # Defines the training algorithm. We will use skip-gram so 1 is chosen.
-    hs=0,  # Set to 0, as we are applying negative sampling.
-    negative=5,  # If > 0, negative sampling will be used. We will use a value of 5.
-    window=25,
-)
+# model = Word2Vec(
+#     sentences=lines,  # We will supply the pre-processed list of moive lists to this parameter
+#     iter=1000,  # epoch
+#     min_count=10,  # a movie has to appear more than 10 times to be keeped
+#     size=1000,  # size of the hidden layer
+#     workers=10,  # specify the number of threads to be used for training
+#     sg=1,  # Defines the training algorithm. We will use skip-gram so 1 is chosen.
+#     hs=0,  # Set to 0, as we are applying negative sampling.
+#     negative=5,  # If > 0, negative sampling will be used. We will use a value of 5.
+#     window=25,
+# )
+
+# model.save("word2vec.model")
 
 
 # X后面跟着Y M后面跟着N
-print(model.wv.most_similar(positive=['X','N','S'], negative=['Y','T']))
+model = Word2Vec.load("word2vec.model")
+print(model.wv.most_similar(positive=['X','M','S','U']))
+print(model.wv.most_similar(positive=['A1','B1','C1']))
+
+
+# print(model.wv.most_similar(positive=['X','M','S'], negative=['Y','N']))
+# print(model.wv.most_similar(positive=['A1','B1','C1'], negative=['A2','B2']))
+# print(model.wv.most_similar(positive=['C1','D1','F1'], negative=['C2','D2']))
+# print(model.wv.most_similar(positive=['X','S','M'], negative=['Y','T']))
+# print(model.wv.most_similar(positive=['X','N','S'], negative=['Y','T']))
+# print(model.wv.most_similar(positive=['X','M','S'], negative=['Y','N']))
+# print(model.wv.most_similar(positive=['X','N','T'], negative=['Y','M']))
 # M后面跟着N S后面跟着T
 # print(model.wv.most_similar(positive=['M','T'], negative=list('N')))
 
